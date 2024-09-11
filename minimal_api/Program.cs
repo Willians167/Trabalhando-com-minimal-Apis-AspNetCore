3e1c86aa38d73e08b4e84e7a30227953496a15ff
Willians <prwillians.costa@gmail.com>
2024-09-11 03:54:07 -0300

    Adiciona Endpoint Post/Login para autenticar usuario adm

--- minimal_api/Program.cs
+++ minimal_api/Program.cs
@@ -3,4 +3,20 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapPost("login",(LoginDTO loginDTO) => {
+    if(loginDTO.Email == "adm@test.com" && loginDTO.Senha == "123456")
+          return Results.Ok("Login com sucesso");
+    else 
+          return Results.Unauthorized();
+         
+
+    
+});
+
+
 app.Run();
+
+public class LoginDTO{
+    public string Email { get; set; } = default;
+    public string Senha { get; set; }  = default;
+}
