c53bf04c0303bc6d799ebd7f6ee4f4831b883adb
Willians <prwillians.costa@gmail.com>
2024-09-25 16:48:42 -0300

    cria MapPut veiculos

--- minimal_api/Program.cs
+++ minimal_api/Program.cs
@@ -70,6 +70,19 @@ if(veiculo == null) return Results.NotFound();
     
     return Results.Ok(veiculo);
 }).WithTags("Veiculo");
+
+app.MapPut("/veiculos/{id}",([FromRoute]int id, VeiculoDTO veiculoDTO,IVeiculoServico veiculoServico) => {
+
+var veiculo = veiculoServico.BuscaPorId(id);
+if(veiculo == null) return Results.NotFound();
+    veiculo.Nome = veiculoDTO.Nome;
+    veiculo.Marca= veiculoDTO.Marca;
+    veiculo.Ano = veiculoDTO.Ano;
+
+    veiculoServico.Atualizar(veiculo);
+   
+    return Results.Ok(veiculo);
+}).WithTags("Veiculo");
 #endregion
 
 #region App
